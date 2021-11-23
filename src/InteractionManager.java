b4c7bf84a1c30c26aa05529b65bc358f82401048
aaronsalot <asalot@macalester.edu>
2021-11-23 12:06:22 -0600

    including the bullet ellipse

--- src/InteractionManager.java
+++ src/InteractionManager.java
@@ -1,9 +1,14 @@
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Ellipse;
 
 public class InteractionManager {
     public void alienIntersection(Bullet bullet, Alien alien, CanvasWindow canvas) {
-        if (canvas.getElementAt(bullet) == alien) {
+        
+        Ellipse Bulletpoint = new Point(Bullet.getCenterX(), Bullet.getCenterY() + Ball.BALL_RADIUS + 1);
 
+        
+        if (canvas.getElementAt(bullet) == alien) {
+            
         }
     }
 }
