3eb0fde56060ea67c26513f772145a1dceb2aae4
aaronsalot <asalot@macalester.edu>
2021-11-23 11:49:32 -0600

    Create InteractionManager.java

---
+++ src/InteractionManager.java
@@ -0,0 +1,10 @@
+import edu.macalester.graphics.CanvasWindow;
+
+public class InteractionManager {
+    public void alienIntersection(Bullet bullet, Alien alien, CanvasWindow canvas) {
+        if (canvas.getElementAt(bullet) == alien) {
+
+        }
+    }
+}
+
