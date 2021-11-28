f90cad39b7fdf5eefa46530b6eb0e83c800588e5
Ethan Brooks <ebrooks1@macalester.edu>
2021-11-28 15:22:07 -0600

    AlienWall now extends graphics group

--- src/AlienWall.java
+++ src/AlienWall.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
-import edu.macalester.graphics.*;
+import edu.macalester.graphics.GraphicsGroup;
+import edu.macalester.graphics.CanvasWindow;
 
-public class AlienWall {
-    private GraphicsGroup alienGroup;
+public class AlienWall extends GraphicsGroup {
+    
     private List<Alien> aliens = new ArrayList<Alien>();
     private static CanvasWindow canvas;
 
 
     public AlienWall(CanvasWindow canvas) {
+        super();
         this.canvas = canvas;
-        alienGroup = new GraphicsGroup();
         generateAlienWall();
-        System.out.print(alienGroup);
-        // canvas.add(aliens);
+        System.out.print(this);
+        canvas.add(this);
     }
 
     public void generateAlienWall() {
@@ -31,34 +32,6 @@ public class AlienWall {
         alien.addToCanvas(canvas);
     }
 
-
-
-
-
-
-
-
-    // private void createalienWall(CanvasWindow canvas) {
-    // double margin = canvas.getWidth() * 0.05;
-    // double spacing = canvas.getWidth() * 0.01;
-    // double y = canvas.getWidth() * 0.05;
-    // double x = margin;
-    // double length = 0;
-    // for (int i = 0; i < 48; i++) {
-    // Alien alienNew = new Alien();
-    // length = length + alienNew.getImageWidth() + spacing;
-    // if (length < canvas.getWidth() - 4 * margin) {
-    // x = x + alienNew.getImageWidth() + spacing;
-    // } else {
-    // x = margin;
-    // length = 0;
-    // y = y + spacing + alienNew.getImageHeight();
-    // }
-    // // alienGroup.add(alienNew);
-    // // aliens.add(alienNew);
-    // canvas.add(alienNew.getAlienImage());
-    // }
-    // }
 
 }
 
